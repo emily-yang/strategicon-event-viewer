d065edd67f33f40578593c3006f7b18aa0c58c0e
emily-yang <cigoua@gmail.com>
2017-08-08 02:59:03 -0700

    hooked up db

--- src/scraper.js
+++ src/scraper.js
@@ -1,1 +1,27 @@
 const jsdom = require("jsdom");
+const mongoose = require("mongoose");
+//const Event = mongoose.model('Event');
+const Event = require('./model/Event');
+
+function parseSchedule(){
+	const { JSDOM } = jsdom;
+	const dom = new JSDOM(``, {
+		url: "http://www.strategicon.net/index.php?goto=events&disp_descs=Y",
+		includeNodeLocations: true
+	});
+	const newEvent = new Event( {
+		title: "sample game",
+		type: "Small Tourney",
+		day: "Sat",
+		startTime: "9:00am",
+		duration: "5",
+		id: "123",
+		gm: "mr guy",
+		desc: "A game with dice"
+	});
+	newEvent.save();
+	console.log('saved');
+	return 'this is hooked up';
+};
+
+export { parseSchedule };
--- src/server.js
+++ src/server.js
@@ -1,11 +1,25 @@
 /* eslint no-console: "off"*/
 
+import session from 'express-session';
 import express from 'express';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter as Router } from 'react-router-dom';
 import App from './components/App';
-import scraper from './scraper';
+import {parseSchedule} from './scraper';
+import bodyParser from 'body-parser';
+
+// import environmental variables from our variables.env file
+require('dotenv').config({ path: 'variables.env' });
+
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo')(session);
+// Connect to our Database and handle an bad connections
+mongoose.connect(process.env.DATABASE);
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+mongoose.connection.on('error', (err) => {
+  console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+});
 
 const app = new express();
 
@@ -16,9 +30,24 @@ app.set('views', `${__dirname}/views`);
 // define the folder that will be used for static assets
 app.use(express.static(`${__dirname}/static`));
 
-app.get('/json', (req,res) => {
-  res.json();
-});
+// Takes the raw requests and turns them into usable properties on req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Sessions allow us to store data on visitors from request to request
+// This keeps users logged in and allows us to send flash messages
+app.use(session({
+  secret: process.env.SECRET,
+  key: process.env.KEY,
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({ mongooseConnection: mongoose.connection })
+}));
+
+app.get('/json', (req, res) => res.send(parseSchedule()));
+
+require('./model/Event');
+
 // universal routing and rendering
 app.get('*', (req, res) => {
   let markup = '';
@@ -37,20 +66,14 @@ app.get('*', (req, res) => {
       return res.redirect(302, context.url);
     }
 
-  return res.render('index', {markup});
+  res.render('index', {markup});
 });
-
 
 // start the server
 const port = process.env.PORT || 3000;
 const env = process.env.NODE_ENV || 'production';
 app.listen(port, (err) => {
-  if (err) {
+  if (err)
     return console.error(err);
-  }
-  return console.info(
-    `
-      Server running on http://localhost:${port} [${env}]
-      Universal rendering: ${process.env.UNIVERSAL ? 'enabled' : 'disabled'}
-    `);
 });
+ 
