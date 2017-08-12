ffda7fd3ea53215425673dcd2edbce4c7d92cf47
emily-yang <cigoua@gmail.com>
2017-08-11 18:32:09 -0700

    save events to db

--- src/scraper.js
+++ src/scraper.js
@@ -15,43 +15,35 @@ function parseSchedule(){
 	).then (dom => {
 		const document = dom.window.document;
 		const listings = document.querySelectorAll('.Board, .RPG');
-		// console.log(listings[2].children[3].textContent);
-		const category = listings[40].classList.contains('Board') ? 'board' : 'rpg';
-		const [day, time, duration] = getDayTimeDuration(listings[40]);
-		const [title, gm, desc] = getTitleGmDesc(listings[40]);
-		
-		const event = new Event({
-			title,
-			category,
-			day,
-			time,
-			duration,
-			gm,
-			desc
-		});
-		event.save();
-		console.log('saving event');
+		for (let i = 0; i < listings.length; i++) {
+			// console.log(listings[2].children[3].textContent);
+			const category = listings[i].classList.contains('Board') ? 'boardgame' : 'rpg';
+			const [day, time, duration] = getDayTimeDuration(listings[i]);
+			const [title, gm, desc] = getTitleGmDesc(listings[i]);
+			
+			const event = new Event({
+				title,
+				category,
+				day,
+				time,
+				duration,
+				gm,
+				desc
+			});
+			event.save();
+			console.log('saving event');
+		}
 	}).catch( (error) => {
 		console.log(error);
 	});
-
-	// const newEvent = new Event( {
-	// 	title: "sample game",
-	// 	category: "Board",
-	// 	day: "Sat",
-	// 	startTime: "9:00am",
-	// 	duration: "5",
-	// 	gm: "mr guy",
-	// 	desc: "A game with dice"
-	// });
-	// newEvent.save();
-
-	//return 'passed through parseSchedule';
 };
 
 function getDayTimeDuration(listing) {
 	// Fri 2:00 pm 4 hrs
-	const info = listing.children[2].textContent.split(' ');
+	const info = listing.children[2].textContent
+		.replace('Noon', '12:00 pm')
+		.replace('Midnight', '12:00 am')
+		.split(' ');
 
 	const [day, time, duration] = [info[0], info[1] + info[2], info[3] + info[4]];
 	return [day, time, duration];
