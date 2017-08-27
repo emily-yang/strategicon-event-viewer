97c6117a61f22fc2f233731b2b0b20c7d75afa00
emily-yang <cigoua@gmail.com>
2017-08-27 00:50:33 -0700

    color by category

--- client/src/Event.js
+++ client/src/Event.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 class Event extends Component {
 	render() {
+		const classList = classNames('event', this.props.event.category);
+
 		return (
-			<div className="event">
+			<div className={classList}>
 				<h3> {this.props.event.title}</h3>
 				<p>GM: {this.props.event.gm}</p>
 				<p>Time: {this.props.event.time}</p>
